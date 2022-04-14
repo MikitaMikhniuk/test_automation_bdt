db8fcca0c60b38d9a641068676b91ca90f8f9105
MikitaMikhniuk <n7sr2rb@gmail.com>
2022-04-15 00:54:27 +0300

    autoformat

--- onliner/page_objects/header/onliner_header.py
+++ onliner/page_objects/header/onliner_header.py
@@ -1,6 +1,7 @@
 from framework.elements.element_factory import ELEMENT_FACTORY, ElementType
 from selenium.webdriver.common.by import By
 
+
 class OnlinerHeader:
 
     CATALOG_TOP_BAR = '//a[contains(@class,"b-main-navigation__link")]/span[text()="{0}"]'
